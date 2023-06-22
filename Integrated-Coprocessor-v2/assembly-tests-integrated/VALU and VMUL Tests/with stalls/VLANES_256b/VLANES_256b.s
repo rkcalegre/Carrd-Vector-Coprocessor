42d13d0689a00e9890f6518ad16b82d5b8c7812a
Kai <70148281+rkcalegre@users.noreply.github.com>
2023-06-22 13:01:46 +0800

    Update VLANES_256b.s

--- Integrated-Coprocessor-v2/assembly-tests-integrated/VALU and VMUL Tests/with stalls/VLANES_256b/VLANES_256b.s
+++ Integrated-Coprocessor-v2/assembly-tests-integrated/VALU and VMUL Tests/with stalls/VLANES_256b/VLANES_256b.s
@@ -83,7 +83,7 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-vand.vi v8, v4, 1
+vand.vi v20, v4, 1
 C.NOP
 C.NOP
 C.NOP
@@ -100,7 +100,7 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v8, x1          # m[x1 = 8] = v16
+vse32.v v20, x1          # m[x1 = 8] = v16
 ADDI x1, x1, 8
 C.NOP
 C.NOP
@@ -118,7 +118,7 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-vor.vi v8, v4, 0
+vor.vi v20, v4, 0
 C.NOP
 C.NOP
 C.NOP
@@ -135,7 +135,7 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v8, x1          # m[x1 = 8] = v16
+vse32.v v20, x1          # m[x1 = 8] = v16
 ADDI x1, x1, 8
 C.NOP
 C.NOP
@@ -153,7 +153,7 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-vxor.vi v8, v4, 0
+vxor.vi v20, v4, 0
 C.NOP
 C.NOP
 C.NOP
@@ -170,7 +170,7 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v8, x1          # m[x1 = 8] = v16
+vse32.v v20, x1          # m[x1 = 8] = v16
 ADDI x1, x1, 8
 C.NOP
 C.NOP
@@ -194,7 +194,7 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-vsll.vi v8, v4, 4
+vsll.vi v20, v4, 4
 vse32.v v12, x1          # m[x1 = 0] = v8
 ADDI x1, x1, 8
 C.NOP
@@ -207,7 +207,7 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v8, x1          # m[x1 = 8] = v16
+vse32.v v20, x1          # m[x1 = 8] = v16
 ADDI x1, x1, 8
 C.NOP
 C.NOP
@@ -225,7 +225,7 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-vsrl.vi v8, v4, 1
+vsrl.vi v20, v4, 1
 C.NOP
 C.NOP
 C.NOP
@@ -242,7 +242,7 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v8, x1          # m[x1 = 8] = v16
+vse32.v v20, x1          # m[x1 = 8] = v16
 ADDI x1, x1, 8
 C.NOP
 C.NOP
@@ -260,7 +260,7 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-vsra.vi v8, v4, 2
+vsra.vi v20, v4, 2
 C.NOP
 C.NOP
 C.NOP
@@ -277,7 +277,7 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v8, x1          # m[x1 = 8] = v16
+vse32.v v20, x1          # m[x1 = 8] = v16
 ADDI x1, x1, 8
 C.NOP
 C.NOP
@@ -339,12 +339,12 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-vadd.vi v8, v0, 2
+vadd.vi v20, v0, 2
 C.NOP
 C.NOP
 C.NOP
 C.NOP
-vmul.vv v12, v8, v8
+vmul.vv v12, v8, v20
 C.NOP
 C.NOP
 C.NOP
