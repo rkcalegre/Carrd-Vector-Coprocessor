d1145df1c482efbc89342b84eb6705e3905fc802
Dimple Mae Manalo <130539054+dnmanalo@users.noreply.github.com>
2023-06-24 19:10:24 +0800

    upload div_gen_unsigned ip

---
+++ rv32imc/vivado-ip-src/div_gen_unsigned/div_gen_unsigned_stub.v
@@ -0,0 +1,32 @@
+// Copyright 1986-2023 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2022.2.2 (win64) Build 3788238 Tue Feb 21 20:00:34 MST 2023
+// Date        : Fri May 26 15:35:15 2023
+// Host        : DimpleMae running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub
+//               c:/Users/Manal/Documents/VivadoProjects/Carrd-Vector-Coprocessor/rv32imc/vivado-ip-src/div_gen_unsigned/div_gen_unsigned_stub.v
+// Design      : div_gen_unsigned
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7a200tfbg676-2
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "div_gen_v5_1_19,Vivado 2022.2.2" *)
+module div_gen_unsigned(aclk, aclken, aresetn, s_axis_divisor_tvalid, 
+  s_axis_divisor_tready, s_axis_divisor_tdata, s_axis_dividend_tvalid, 
+  s_axis_dividend_tready, s_axis_dividend_tdata, m_axis_dout_tvalid, m_axis_dout_tdata)
+/* synthesis syn_black_box black_box_pad_pin="aclk,aclken,aresetn,s_axis_divisor_tvalid,s_axis_divisor_tready,s_axis_divisor_tdata[31:0],s_axis_dividend_tvalid,s_axis_dividend_tready,s_axis_dividend_tdata[31:0],m_axis_dout_tvalid,m_axis_dout_tdata[63:0]" */;
+  input aclk;
+  input aclken;
+  input aresetn;
+  input s_axis_divisor_tvalid;
+  output s_axis_divisor_tready;
+  input [31:0]s_axis_divisor_tdata;
+  input s_axis_dividend_tvalid;
+  output s_axis_dividend_tready;
+  input [31:0]s_axis_dividend_tdata;
+  output m_axis_dout_tvalid;
+  output [63:0]m_axis_dout_tdata;
+endmodule
