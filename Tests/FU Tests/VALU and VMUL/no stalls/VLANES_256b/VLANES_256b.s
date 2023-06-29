3452872eb3b04bff35e0e27c86751194119e3e24
Kai <70148281+rkcalegre@users.noreply.github.com>
2023-06-30 03:48:43 +0800

    Update VLANES_256b.s

--- Tests/FU Tests/VALU and VMUL/no stalls/VLANES_256b/VLANES_256b.s
+++ Tests/FU Tests/VALU and VMUL/no stalls/VLANES_256b/VLANES_256b.s
@@ -9,185 +9,185 @@ C.NOP
 ADDI x1, x0, 0          # Address Pointer
 ADDI x2, x0, 10         # Scalar Operand for VX instructions
 vadd.vi v4, v0, 15      # v4 = 15
-vadd.vi v5, v0, 20      # v5 = 20
-vadd.vv v6, v5, v4      # v6 = v4 + v5
-vadd.vx v7, v4, x2      # v7 = v4 + x2
-vse32.v v5, x1          # m[x1 = 0] = v5 = 20
+vadd.vi v8, v0, 20      # v8 = 20
+vadd.vv v12, v8, v4      # v12 = v4 + v8
+vadd.vx v16, v4, x2      # v16 = v4 + x2
+vse32.v v8, x1          # m[x1 = 0] = v8 = 20
 ADDI x1, x1, 8
 C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v6, x1          # m[x1 = 4] = v6 = 35
+vse32.v v12, x1          # m[x1 = 4] = v12 = 35
 ADDI x1, x1, 8
 C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v7, x1          # m[x1 = 8] = v7 = 25
+vse32.v v16, x1          # m[x1 = 8] = v16 = 25
 ADDI x1, x1, 8
 C.NOP
 C.NOP
 
 # VSUB
-vsub.vv v6, v4, v5      # v4 = 15
-vsub.vx v7, v4, x2      # v5 = 20
-vse32.v v6, x1          # m[x1 = 12] = v5
+vsub.vv v12, v4, v8      # v4 = 15
+vsub.vx v16, v4, x2      # v8 = 20
+vse32.v v12, x1          # m[x1 = 12] = v8
 ADDI x1, x1, 8
 C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v7, x1          # m[x1 = 16] = v6
+vse32.v v16, x1          # m[x1 = 16] = v12
 ADDI x1, x1, 8
 C.NOP
 
 # VAND
-vand.vv v6, v5, v4     
-vand.vx v7, v4, x2      
-vand.vi v8, v4, 1
-vse32.v v6, x1          # m[x1 = 0] = v5
+vand.vv v12, v8, v4     
+vand.vx v16, v4, x2      
+vand.vi v20, v4, 1
+vse32.v v12, x1          # m[x1 = 0] = v8
 ADDI x1, x1, 8
 C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v7, x1          # m[x1 = 4] = v6
+vse32.v v16, x1          # m[x1 = 4] = v12
 ADDI x1, x1, 8
 C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v8, x1          # m[x1 = 8] = v7
+vse32.v v20, x1          # m[x1 = 8] = v16
 ADDI x1, x1, 8
 C.NOP
 
 # VOR
-vor.vv v6, v5, v4     
-vor.vx v7, v4, x2      
-vor.vi v8, v4, 0
-vse32.v v6, x1          # m[x1 = 0] = v5
+vor.vv v12, v8, v4     
+vor.vx v16, v4, x2      
+vor.vi v20, v4, 0
+vse32.v v12, x1          # m[x1 = 0] = v8
 ADDI x1, x1, 8
 C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v7, x1          # m[x1 = 4] = v6
+vse32.v v16, x1          # m[x1 = 4] = v12
 ADDI x1, x1, 8
 C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v8, x1          # m[x1 = 8] = v7
+vse32.v v20, x1          # m[x1 = 8] = v16
 ADDI x1, x1, 8
 C.NOP
 
 # VXOR
-vxor.vv v6, v5, v4     
-vxor.vx v7, v4, x2      
-vxor.vi v8, v4, 0
-vse32.v v6, x1          # m[x1 = 0] = v5
+vxor.vv v12, v8, v4     
+vxor.vx v16, v4, x2      
+vxor.vi v20, v4, 0
+vse32.v v12, x1          # m[x1 = 0] = v8
 ADDI x1, x1, 8
 C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v7, x1          # m[x1 = 4] = v6
+vse32.v v16, x1          # m[x1 = 4] = v12
 ADDI x1, x1, 8
 C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v8, x1          # m[x1 = 8] = v7
+vse32.v v20, x1          # m[x1 = 8] = v16
 ADDI x1, x1, 8
 
 # VSLL
 ADDI x3, x0, 3
-vadd.vi v9, v0, 2
-vsll.vv v6, v5, v9     
-vsll.vx v7, v4, x3      
-vsll.vi v8, v4, 4
-vse32.v v6, x1          # m[x1 = 0] = v5
+vadd.vi v28, v0, 2
+vsll.vv v12, v8, v28     
+vsll.vx v16, v4, x3      
+vsll.vi v20, v4, 4
+vse32.v v12, x1          # m[x1 = 0] = v8
 ADDI x1, x1, 8
 C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v7, x1          # m[x1 = 4] = v6
+vse32.v v16, x1          # m[x1 = 4] = v12
 ADDI x1, x1, 8
 C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v8, x1          # m[x1 = 8] = v7
+vse32.v v20, x1          # m[x1 = 8] = v16
 ADDI x1, x1, 8
 C.NOP
 
 # VSRL
-vsrl.vv v6, v5, v9    
-vsrl.vx v7, v4, x3      
-vsrl.vi v8, v4, 1
-vse32.v v6, x1          # m[x1 = 0] = v5
+vsrl.vv v12, v8, v9    
+vsrl.vx v16, v4, x3      
+vsrl.vi v20, v4, 1
+vse32.v v12, x1          # m[x1 = 0] = v8
 ADDI x1, x1, 8
 C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v7, x1          # m[x1 = 4] = v6
+vse32.v v16, x1          # m[x1 = 4] = v12
 ADDI x1, x1, 8
 C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v8, x1          # m[x1 = 8] = v7
+vse32.v v20, x1          # m[x1 = 8] = v16
 ADDI x1, x1, 8
 C.NOP
 
 # VSRA
-vsra.vv v6, v5, v9     
-vsra.vx v7, v4, x3      
-vsra.vi v8, v4, 2
-vse32.v v6, x1          # m[x1 = 0] = v5
+vsra.vv v12, v8, v9     
+vsra.vx v16, v4, x3      
+vsra.vi v20, v4, 2
+vse32.v v12, x1          # m[x1 = 0] = v8
 ADDI x1, x1, 8
 C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v7, x1          # m[x1 = 4] = v6
+vse32.v v16, x1          # m[x1 = 4] = v12
 ADDI x1, x1, 8
 C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v8, x1          # m[x1 = 8] = v7
+vse32.v v20, x1          # m[x1 = 8] = v16
 ADDI x1, x1, 8
 C.NOP
 C.NOP
 
 # VMIN
-vmin.vv v6, v5, v4
-vmin.vx v7, v4, x2
-vse32.v v6, x1          # m[x1 = 0] = v5
+vmin.vv v12, v8, v4
+vmin.vx v16, v4, x2
+vse32.v v12, x1          # m[x1 = 0] = v8
 ADDI x1, x1, 8
 C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v7, x1          # m[x1 = 4] = v6
+vse32.v v16, x1          # m[x1 = 4] = v12
 ADDI x1, x1, 8
 C.NOP
 C.NOP
 
 # VMAX
-vmax.vv v6, v5, v4
-vmax.vx v7, v4, x2
-vse32.v v6, x1          # m[x1 = 0] = v5
+vmax.vv v12, v8, v4
+vmax.vx v16, v4, x2
+vse32.v v12, x1          # m[x1 = 0] = v8
 ADDI x1, x1, 8
 C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v7, x1          # m[x1 = 4] = v6
+vse32.v v16, x1          # m[x1 = 4] = v12
 ADDI x1, x1, 8
 
 ################### VMUL TESTS ######################
@@ -196,15 +196,15 @@ ADDI x3, x0, 2
 C.NOP
 C.NOP
 vadd.vi v8, v0, 2
-vmul.vv v6, v5, v8
-vmul.vx v7, v4, x3
-vse32.v v6, x1          # m[x1 = 0] = v5
+vmul.vv v12, v8, v8
+vmul.vx v16, v4, x3
+vse32.v v12, x1          # m[x1 = 0] = v8
 ADDI x1, x1, 8
 C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v7, x1          # m[x1 = 4] = v6
+vse32.v v16, x1          # m[x1 = 4] = v12
 ADDI x1, x1, 8
 C.NOP
 C.NOP
