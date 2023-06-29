3ef47ee46ae403e0a77951b46572d84b7e587dd3
Kai <70148281+rkcalegre@users.noreply.github.com>
2023-06-30 07:05:35 +0800

    Update VLANES_512b.s

--- Tests/FU Tests/VALU and VMUL/no stalls/VLANES_512b/VLANES_512b.s
+++ Tests/FU Tests/VALU and VMUL/no stalls/VLANES_512b/VLANES_512b.s
@@ -1,6 +1,7 @@
 ADDI x5, x0, 16         # x5 = 16
 vsetivli x20, x5, 18    # 32 bit elements 256-bit vector
 ADDI x5, x5, 16         # x5 = x5 + 16 = 32
+vle32.v x28, x0
 C.NOP
 C.NOP
 C.NOP
@@ -19,12 +20,20 @@ C.NOP
 C.NOP
 C.NOP
 vse32.v v12, x1          # m[x1 = 4] = v12 = 35
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 ADDI x1, x1, 16
 C.NOP
 C.NOP
 C.NOP
 C.NOP
 vse32.v v16, x1          # m[x1 = 8] = v16 = 25
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 ADDI x1, x1, 16         
 C.NOP
 C.NOP
@@ -39,6 +48,10 @@ C.NOP
 C.NOP
 C.NOP
 vse32.v v16, x1          # m[x1 = 16] = v12
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 ADDI x1, x1, 16
 C.NOP
 
@@ -53,12 +66,20 @@ C.NOP
 C.NOP
 C.NOP
 vse32.v v16, x1          # m[x1 = 4] = v12
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 ADDI x1, x1, 16
 C.NOP
 C.NOP
 C.NOP
 C.NOP
 vse32.v v20, x1          # m[x1 = 8] = v16
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 ADDI x1, x1, 16
 C.NOP
 
@@ -67,18 +88,30 @@ vor.vv v12, v8, v4
 vor.vx v16, v4, x2      
 vor.vi v20, v4, 0
 vse32.v v12, x1          # m[x1 = 0] = v8
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 ADDI x1, x1, 16
 C.NOP
 C.NOP
 C.NOP
 C.NOP
 vse32.v v16, x1          # m[x1 = 4] = v12
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 ADDI x1, x1, 16
 C.NOP
 C.NOP
 C.NOP
 C.NOP
 vse32.v v20, x1          # m[x1 = 8] = v16
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 ADDI x1, x1, 16
 C.NOP
 
@@ -87,18 +120,30 @@ vxor.vv v12, v8, v4
 vxor.vx v16, v4, x2      
 vxor.vi v20, v4, 0
 vse32.v v12, x1          # m[x1 = 0] = v8
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 ADDI x1, x1, 16
 C.NOP
 C.NOP
 C.NOP
 C.NOP
 vse32.v v16, x1          # m[x1 = 4] = v12
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 ADDI x1, x1, 16
 C.NOP
 C.NOP
 C.NOP
 C.NOP
 vse32.v v20, x1          # m[x1 = 8] = v16
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 ADDI x1, x1, 16
 
 # VSLL
@@ -108,18 +153,30 @@ vsll.vv v12, v8, v28
 vsll.vx v16, v4, x3      
 vsll.vi v20, v4, 4
 vse32.v v12, x1          # m[x1 = 0] = v8
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 ADDI x1, x1, 16
 C.NOP
 C.NOP
 C.NOP
 C.NOP
 vse32.v v16, x1          # m[x1 = 4] = v12
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 ADDI x1, x1, 16
 C.NOP
 C.NOP
 C.NOP
 C.NOP
 vse32.v v20, x1          # m[x1 = 8] = v16
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 ADDI x1, x1, 16
 C.NOP
 
@@ -128,18 +185,30 @@ vsrl.vv v12, v8, v28
 vsrl.vx v16, v4, x3      
 vsrl.vi v20, v4, 1
 vse32.v v12, x1          # m[x1 = 0] = v8
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 ADDI x1, x1, 16
 C.NOP
 C.NOP
 C.NOP
 C.NOP
 vse32.v v16, x1          # m[x1 = 4] = v12
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 ADDI x1, x1, 16
 C.NOP
 C.NOP
 C.NOP
 C.NOP
 vse32.v v20, x1          # m[x1 = 8] = v16
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 ADDI x1, x1, 16
 C.NOP
 
@@ -148,18 +217,30 @@ vsra.vv v12, v8, v28
 vsra.vx v16, v4, x3      
 vsra.vi v20, v4, 2
 vse32.v v12, x1          # m[x1 = 0] = v8
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 ADDI x1, x1, 16
 C.NOP
 C.NOP
 C.NOP
 C.NOP
 vse32.v v16, x1          # m[x1 = 4] = v12
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 ADDI x1, x1, 16
 C.NOP
 C.NOP
 C.NOP
 C.NOP
 vse32.v v20, x1          # m[x1 = 8] = v16
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 ADDI x1, x1, 16
 C.NOP
 C.NOP
@@ -168,12 +249,20 @@ C.NOP
 vmin.vv v12, v8, v4
 vmin.vx v16, v4, x2
 vse32.v v12, x1          # m[x1 = 0] = v8
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 ADDI x1, x1, 16
 C.NOP
 C.NOP
 C.NOP
 C.NOP
 vse32.v v16, x1          # m[x1 = 4] = v12
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 ADDI x1, x1, 16
 C.NOP
 C.NOP
@@ -182,12 +271,20 @@ C.NOP
 vmax.vv v12, v8, v4
 vmax.vx v16, v4, x2
 vse32.v v12, x1          # m[x1 = 0] = v8
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 ADDI x1, x1, 16
 C.NOP
 C.NOP
 C.NOP
 C.NOP
 vse32.v v16, x1          # m[x1 = 4] = v12
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 ADDI x1, x1, 16
 
 ################### VMUL TESTS ######################
@@ -198,8 +295,16 @@ vadd.vi v20, v0, 2
 vmul.vv v12, v8, v20
 vmul.vx v16, v4, x3
 vse32.v v12, x1          # m[x1 = 0] = v8
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 ADDI x1, x1, 16
 vse32.v v16, x1          # m[x1 = 4] = v12
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 ADDI x1, x1, 16
 C.NOP
 C.NOP
