5d2db0c943e73aa147db4546758b654b94e1ee18
Kai <70148281+rkcalegre@users.noreply.github.com>
2023-06-30 03:22:41 +0800

    Update VLANES_512b.s

--- Tests/FU Tests/VALU and VMUL/no stalls/VLANES_512b/VLANES_512b.s
+++ Tests/FU Tests/VALU and VMUL/no stalls/VLANES_512b/VLANES_512b.s
@@ -14,18 +14,33 @@ vadd.vv v6, v5, v4      # v6 = v4 + v5
 vadd.vx v7, v4, x2      # v7 = v4 + x2
 vse32.v v5, x1          # m[x1 = 0] = v5 = 20
 ADDI x1, x1, 16
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v6, x1          # m[x1 = 4] = v6 = 35
 ADDI x1, x1, 16
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v7, x1          # m[x1 = 8] = v7 = 25
 ADDI x1, x1, 16         
+C.NOP
+C.NOP
 
 # VSUB
 vsub.vv v6, v4, v5      # v4 = 15
 vsub.vx v7, v4, x2      # v5 = 20
 vse32.v v6, x1          # m[x1 = 12] = v5
 ADDI x1, x1, 16
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v7, x1          # m[x1 = 16] = v6
 ADDI x1, x1, 16
+C.NOP
 
 # VAND
 vand.vv v6, v5, v4     
@@ -33,10 +48,19 @@ vand.vx v7, v4, x2
 vand.vi v8, v4, 1
 vse32.v v6, x1          # m[x1 = 0] = v5
 ADDI x1, x1, 16
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v7, x1          # m[x1 = 4] = v6
 ADDI x1, x1, 16
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v8, x1          # m[x1 = 8] = v7
 ADDI x1, x1, 16
+C.NOP
 
 # VOR
 vor.vv v6, v5, v4     
@@ -44,10 +68,19 @@ vor.vx v7, v4, x2
 vor.vi v8, v4, 0
 vse32.v v6, x1          # m[x1 = 0] = v5
 ADDI x1, x1, 16
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v7, x1          # m[x1 = 4] = v6
 ADDI x1, x1, 16
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v8, x1          # m[x1 = 8] = v7
 ADDI x1, x1, 16
+C.NOP
 
 # VXOR
 vxor.vv v6, v5, v4     
@@ -55,8 +88,16 @@ vxor.vx v7, v4, x2
 vxor.vi v8, v4, 0
 vse32.v v6, x1          # m[x1 = 0] = v5
 ADDI x1, x1, 16
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v7, x1          # m[x1 = 4] = v6
 ADDI x1, x1, 16
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v8, x1          # m[x1 = 8] = v7
 ADDI x1, x1, 16
 
@@ -68,10 +109,19 @@ vsll.vx v7, v4, x3
 vsll.vi v8, v4, 4
 vse32.v v6, x1          # m[x1 = 0] = v5
 ADDI x1, x1, 16
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v7, x1          # m[x1 = 4] = v6
 ADDI x1, x1, 16
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v8, x1          # m[x1 = 8] = v7
 ADDI x1, x1, 16
+C.NOP
 
 # VSRL
 vsrl.vv v6, v5, v9    
@@ -79,10 +129,19 @@ vsrl.vx v7, v4, x3
 vsrl.vi v8, v4, 1
 vse32.v v6, x1          # m[x1 = 0] = v5
 ADDI x1, x1, 16
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v7, x1          # m[x1 = 4] = v6
 ADDI x1, x1, 16
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v8, x1          # m[x1 = 8] = v7
 ADDI x1, x1, 16
+C.NOP
 
 # VSRA
 vsra.vv v6, v5, v9     
@@ -90,30 +149,51 @@ vsra.vx v7, v4, x3
 vsra.vi v8, v4, 2
 vse32.v v6, x1          # m[x1 = 0] = v5
 ADDI x1, x1, 16
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v7, x1          # m[x1 = 4] = v6
 ADDI x1, x1, 16
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v8, x1          # m[x1 = 8] = v7
 ADDI x1, x1, 16
+C.NOP
+C.NOP
 
 # VMIN
 vmin.vv v6, v5, v4
 vmin.vx v7, v4, x2
 vse32.v v6, x1          # m[x1 = 0] = v5
 ADDI x1, x1, 16
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v7, x1          # m[x1 = 4] = v6
 ADDI x1, x1, 16
+C.NOP
+C.NOP
 
 # VMAX
 vmax.vv v6, v5, v4
 vmax.vx v7, v4, x2
 vse32.v v6, x1          # m[x1 = 0] = v5
 ADDI x1, x1, 16
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v7, x1          # m[x1 = 4] = v6
 ADDI x1, x1, 16
 
 ################### VMUL TESTS ######################
 # VMUL
 ADDI x3, x0, 2
+C.NOP
 vadd.vi v8, v0, 2
 vmul.vv v6, v5, v8
 vmul.vx v7, v4, x3
@@ -121,3 +201,23 @@ vse32.v v6, x1          # m[x1 = 0] = v5
 ADDI x1, x1, 16
 vse32.v v7, x1          # m[x1 = 4] = v6
 ADDI x1, x1, 16
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
