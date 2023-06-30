f7797cc3d40e5f200b23e0175ee2f1f3ce916ed4
Celine Joy Capua <130539235+cacapua@users.noreply.github.com>
2023-07-01 04:36:26 +0800

    Update multiplication.s

--- Tests/benchmark/multiplication/with stalls/multiplication.s
+++ Tests/benchmark/multiplication/with stalls/multiplication.s
@@ -2,61 +2,61 @@ ADDI x5, x0, 16         # x5 = 16
 vsetivli x20, x5, 18    # 32 bit elements 512-bit vector
 ADDI x1, x0, 16
 ADDI x2, x0, 32
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
+C.NOP
+C.NOP
+C.NOP
 vle32.v v0, x0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vle32.v v4, x1
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vmul.vv v8, v0, v4
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v8, x0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vsetivli x20, x5, 10    # 16 bit elements 512-bit vector
-ADDI x0, x0, 0
-ADDI x0, x0, 0
+C.NOP
+C.NOP
 vmul.vv v8, v0, v4
 ADDI x1, x0, 16
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v8, x1
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vsetivli x20, x5, 2     # 8 bit elements 512-bit vector
-ADDI x0, x0, 0
-ADDI x0, x0, 0
+C.NOP
+C.NOP
 vmul.vv v8, v0, v4
 ADDI x2, x0, 32
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v8, x2
 C.NOP
 C.NOP
