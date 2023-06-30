1045b0be3adeeb0ce214eaf05d8e5081dbdae4e6
Celine Joy Capua <130539235+cacapua@users.noreply.github.com>
2023-06-30 19:50:48 +0800

    Update vred.s

--- Tests/FU Tests/VRED/no stalls/vred.s
+++ Tests/FU Tests/VRED/no stalls/vred.s
@@ -2,321 +2,161 @@ ADDI x5, x0, 16
 #512-bits vred tests
 vsetivli x20, x5, 18 #32 bit elements 512-bit vector
 ADDI x5, x5, 16
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
 vle32.v v4, x0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
 vle32.v v8, x0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
 vredmax.vs v8, v4, v0
 ADDI x1, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v8, x1
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
 vredsum.vs v12, v4, v0
 ADDI x1, x1, 16
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v12, x1
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
 vsetivli x20, x5, 10 #16 bit elements 512-bit vector
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
 vredmax.vs v16, v4, v0
 ADDI x1, x1, 16
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v16, x1
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
 vredsum.vs v20, v4, v0
 ADDI x1, x1, 16
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v20, x1
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
 vsetivli x20, x5, 2 #8 bit elements 512-bit vector
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
 vredmax.vs v24, v4, v0
 ADDI x1, x1, 16
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v24, x1
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
 vredsum.vs v28, v4, v0
 ADDI x1, x1, 16
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v28, x1
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
 #256-bits vred tests
 vsetivli x20, x5, 17 #32 bit elements 256-bit vector
 ADDI x5, x5, 16
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
+C.NOP
+C.NOP
+C.NOP
 vredmax.vs v8, v4, v0
 ADDI x1, x1, 16
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v8, x1
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
 vredsum.vs v12, v4, v0
 ADDI x1, x1, 16
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v12, x1
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
 vsetivli x20, x5, 9 #16 bit elements 256-bit vector
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
 vredmax.vs v16, v4, v0
 ADDI x1, x1, 16
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v16, x1
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
 vredsum.vs v20, v4, v0
 ADDI x1, x1, 16
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v20, x1
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
 vsetivli x20, x5, 1 #8 bit elements 256-bit vector
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
 vredmax.vs v24, v4, v0
 ADDI x1, x1, 16
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v24, x1
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
 vredsum.vs v28, v4, v0
 ADDI x1, x1, 16
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v28, x1
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
 #128-bits vred tests
 vsetivli x20, x5, 16 #32 bit elements 128-bit vector
 ADDI x5, x5, 16
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
+C.NOP
+C.NOP
+C.NOP
 vredmax.vs v8, v4, v0
 ADDI x1, x1, 16
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v8, x1
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
 vredsum.vs v12, v4, v0
 ADDI x1, x1, 16
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v12, x1
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
 vsetivli x20, x5, 8 #16 bit elements 128-bit vector
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
 vredmax.vs v16, v4, v0
 ADDI x1, x1, 16
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v16, x1
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
 vredsum.vs v20, v4, v0
 ADDI x1, x1, 16
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v20, x1
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
 vsetivli x20, x5, 0 #8 bit elements 128-bit vector
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
 vredmax.vs v24, v4, v0
 ADDI x1, x1, 16
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v24, x1
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
 vredsum.vs v28, v4, v0
 ADDI x1, x1, 16
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 vse32.v v28, x1
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
 C.NOP
 C.NOP
 C.NOP
