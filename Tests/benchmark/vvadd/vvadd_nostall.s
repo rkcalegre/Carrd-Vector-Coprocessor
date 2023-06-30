00a65e7dac99a984305c4ca34d4ca2fff804d4bb
Celine Joy Capua <130539235+cacapua@users.noreply.github.com>
2023-07-01 04:25:15 +0800

    Create vvadd_nostall.s

---
+++ Tests/benchmark/vvadd/vvadd_nostall.s
@@ -0,0 +1,33 @@
+ADDI x5, x0, 16         # x5 = 16
+vsetivli x20, x5, 18    # 32 bit elements 512-bit vector
+ADDI x1, x0, 16
+ADDI x2, x0, 32
+vle32.v v0, x0
+vle32.v v4, x1
+vadd.vv v8, v0, v4
+vse32.v v8, x0
+vsetivli x20, x5, 10    # 16 bit elements 512-bit vector
+vadd.vv v8, v0, v4
+ADDI x1, x0, 16
+vse32.v v8, x1
+vsetivli x20, x5, 2     # 8 bit elements 512-bit vector
+vadd.vv v8, v0, v4
+ADDI x2, x0, 32
+vse32.v v8, x2
+ADDI x5, x0, 80
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
