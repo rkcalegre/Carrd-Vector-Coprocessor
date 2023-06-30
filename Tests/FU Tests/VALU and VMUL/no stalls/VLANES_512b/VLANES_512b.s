b41b7d72ebc72b5e009cabfc07b1e378fb2e87ce
Celine Joy Capua <130539235+cacapua@users.noreply.github.com>
2023-07-01 00:54:02 +0800

    Update VLANES_512b.s

--- Tests/FU Tests/VALU and VMUL/no stalls/VLANES_512b/VLANES_512b.s
+++ Tests/FU Tests/VALU and VMUL/no stalls/VLANES_512b/VLANES_512b.s
@@ -19,7 +19,7 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v12, x1          # m[x1 = 4] = v12 = 35
+vse32.v v12, x1          # m[x1 = 10] = v12 = 35
 C.NOP
 C.NOP
 C.NOP
@@ -29,7 +29,7 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v16, x1          # m[x1 = 8] = v16 = 25
+vse32.v v16, x1          # m[x1 = 20] = v16 = 25
 C.NOP
 C.NOP
 C.NOP
@@ -41,13 +41,13 @@ C.NOP
 # VSUB
 vsub.vv v12, v4, v8      # v4 = 15
 vsub.vx v16, v4, x2      # v8 = 20
-vse32.v v12, x1          # m[x1 = 12] = v8
+vse32.v v12, x1          # m[x1 = 30] = v8
 ADDI x1, x1, 16
 C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v16, x1          # m[x1 = 16] = v12
+vse32.v v16, x1          # m[x1 = 40] = v12
 C.NOP
 C.NOP
 C.NOP
@@ -59,13 +59,13 @@ C.NOP
 vand.vv v12, v8, v4     
 vand.vx v16, v4, x2      
 vand.vi v20, v4, 1
-vse32.v v12, x1          # m[x1 = 0] = v8
+vse32.v v12, x1          # m[x1 = 50] = v8
 ADDI x1, x1, 16
 C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v16, x1          # m[x1 = 4] = v12
+vse32.v v16, x1          # m[x1 = 60] = v12
 C.NOP
 C.NOP
 C.NOP
@@ -75,7 +75,7 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v20, x1          # m[x1 = 8] = v16
+vse32.v v20, x1          # m[x1 = 70] = v16
 C.NOP
 C.NOP
 C.NOP
@@ -87,7 +87,7 @@ C.NOP
 vor.vv v12, v8, v4     
 vor.vx v16, v4, x2      
 vor.vi v20, v4, 0
-vse32.v v12, x1          # m[x1 = 0] = v8
+vse32.v v12, x1          # m[x1 = 80] = v8
 C.NOP
 C.NOP
 C.NOP
@@ -97,7 +97,7 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v16, x1          # m[x1 = 4] = v12
+vse32.v v16, x1          # m[x1 = 90] = v12
 C.NOP
 C.NOP
 C.NOP
@@ -107,7 +107,7 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v20, x1          # m[x1 = 8] = v16
+vse32.v v20, x1          # m[x1 = a0] = v16
 C.NOP
 C.NOP
 C.NOP
@@ -119,7 +119,7 @@ C.NOP
 vxor.vv v12, v8, v4     
 vxor.vx v16, v4, x2      
 vxor.vi v20, v4, 0
-vse32.v v12, x1          # m[x1 = 0] = v8
+vse32.v v12, x1          # m[x1 = b0] = v8
 C.NOP
 C.NOP
 C.NOP
@@ -129,7 +129,7 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v16, x1          # m[x1 = 4] = v12
+vse32.v v16, x1          # m[x1 = c0] = v12
 C.NOP
 C.NOP
 C.NOP
@@ -139,7 +139,7 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v20, x1          # m[x1 = 8] = v16
+vse32.v v20, x1          # m[x1 = d0] = v16
 C.NOP
 C.NOP
 C.NOP
@@ -152,7 +152,7 @@ vadd.vi v28, v0, 2
 vsll.vv v12, v8, v28     
 vsll.vx v16, v4, x3      
 vsll.vi v20, v4, 4
-vse32.v v12, x1          # m[x1 = 0] = v8
+vse32.v v12, x1          # m[x1 = e0] = v8
 C.NOP
 C.NOP
 C.NOP
@@ -162,7 +162,7 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v16, x1          # m[x1 = 4] = v12
+vse32.v v16, x1          # m[x1 = f0] = v12
 C.NOP
 C.NOP
 C.NOP
@@ -172,7 +172,7 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v20, x1          # m[x1 = 8] = v16
+vse32.v v20, x1          # m[x1 = 100] = v16
 C.NOP
 C.NOP
 C.NOP
@@ -184,7 +184,7 @@ C.NOP
 vsrl.vv v12, v8, v28    
 vsrl.vx v16, v4, x3      
 vsrl.vi v20, v4, 1
-vse32.v v12, x1          # m[x1 = 0] = v8
+vse32.v v12, x1          # m[x1 = 110] = v8
 C.NOP
 C.NOP
 C.NOP
@@ -194,7 +194,7 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v16, x1          # m[x1 = 4] = v12
+vse32.v v16, x1          # m[x1 = 120] = v12
 C.NOP
 C.NOP
 C.NOP
@@ -204,7 +204,7 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v20, x1          # m[x1 = 8] = v16
+vse32.v v20, x1          # m[x1 = 130] = v16
 C.NOP
 C.NOP
 C.NOP
@@ -216,7 +216,7 @@ C.NOP
 vsra.vv v12, v8, v28     
 vsra.vx v16, v4, x3      
 vsra.vi v20, v4, 2
-vse32.v v12, x1          # m[x1 = 0] = v8
+vse32.v v12, x1          # m[x1 = 140] = v8
 C.NOP
 C.NOP
 C.NOP
@@ -226,7 +226,7 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v16, x1          # m[x1 = 4] = v12
+vse32.v v16, x1          # m[x1 = 150] = v12
 C.NOP
 C.NOP
 C.NOP
@@ -236,7 +236,7 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v20, x1          # m[x1 = 8] = v16
+vse32.v v20, x1          # m[x1 = 160] = v16
 C.NOP
 C.NOP
 C.NOP
@@ -248,7 +248,7 @@ C.NOP
 # VMIN
 vmin.vv v12, v8, v4
 vmin.vx v16, v4, x2
-vse32.v v12, x1          # m[x1 = 0] = v8
+vse32.v v12, x1          # m[x1 = 170] = v8
 C.NOP
 C.NOP
 C.NOP
@@ -258,7 +258,7 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v16, x1          # m[x1 = 4] = v12
+vse32.v v16, x1          # m[x1 = 180] = v12
 C.NOP
 C.NOP
 C.NOP
@@ -270,7 +270,7 @@ C.NOP
 # VMAX
 vmax.vv v12, v8, v4
 vmax.vx v16, v4, x2
-vse32.v v12, x1          # m[x1 = 0] = v8
+vse32.v v12, x1          # m[x1 = 190] = v8
 C.NOP
 C.NOP
 C.NOP
@@ -280,7 +280,7 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v16, x1          # m[x1 = 4] = v12
+vse32.v v16, x1          # m[x1 = 1a0] = v12
 C.NOP
 C.NOP
 C.NOP
@@ -292,13 +292,13 @@ vadd.vi v28, v0, 20
 vmseq.vv v12, v8, v28
 vmseq.vx v16, v4, x2
 vmseq.vi v20, v4, 15
-vse32.v v12, x1
+vse32.v v12, x1          # m[x1 = 1b0] = v12
 addi x1, x1, 16
 C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v16, x1
+vse32.v v16, x1          # m[x1 = 1c0] = v12
 C.NOP
 C.NOP
 C.NOP
@@ -308,7 +308,7 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v20, x1
+vse32.v v20, x1          # m[x1 = 1d0] = v12
 C.NOP
 C.NOP
 C.NOP
@@ -320,7 +320,7 @@ C.NOP
 vmsne.vv v12, v8, v28
 vmsne.vx v16, v4, x2
 vmsne.vi v20, v4, 15
-vse32.v v12, x1
+vse32.v v12, x1          # m[x1 = 1e0] = v12
 C.NOP
 C.NOP
 C.NOP
@@ -330,7 +330,7 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v16, x1
+vse32.v v16, x1          # m[x1 = 1f0] = v12
 C.NOP
 C.NOP
 C.NOP
@@ -340,7 +340,7 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v20, x1
+vse32.v v20, x1          # m[x1 = 200] = v12
 C.NOP
 C.NOP
 C.NOP
@@ -350,9 +350,9 @@ C.NOP
 C.NOP
 
 # VMSLT
-vmslt.vv v12, v4, v5
+vmslt.vv v12, v4, v8
 vmslt.vx v16, v4, x2
-vse32.v v12, x1
+vse32.v v12, x1          # m[x1 = 210] = v12
 C.NOP
 C.NOP
 C.NOP
@@ -362,7 +362,7 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v16, x1
+vse32.v v16, x1          # m[x1 = 220] = v12
 C.NOP
 C.NOP
 C.NOP
@@ -374,7 +374,7 @@ C.NOP
 vmsle.vv v12, v4, v8
 vmsle.vx v16, v4, x2
 vmsle.vi v20, v4, 15
-vse32.v v12, x1
+vse32.v v12, x1          # m[x1 = 230] = v12
 C.NOP
 C.NOP
 C.NOP
@@ -384,7 +384,7 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v16, x1
+vse32.v v16, x1          # m[x1 = 240] = v12
 C.NOP
 C.NOP
 C.NOP
@@ -394,7 +394,7 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v20, x1
+vse32.v v20, x1          # m[x1 = 250] = v12
 C.NOP
 C.NOP
 C.NOP
@@ -408,7 +408,7 @@ C.NOP
 # VMSGT
 vmsgt.vx v12, v4, x2
 vmsgt.vi v16, v4, 10
-vse32.v v12, x1
+vse32.v v12, x1          # m[x1 = 260] = v12
 C.NOP
 C.NOP
 C.NOP
@@ -418,7 +418,7 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-vse32.v v16, x1
+vse32.v v16, x1          # m[x1 = 270] = v12
 C.NOP
 C.NOP
 C.NOP
@@ -432,13 +432,13 @@ C.NOP
 vadd.vi v20, v0, 2
 vmul.vv v12, v8, v20
 vmul.vx v16, v4, x3
-vse32.v v12, x1          # m[x1 = 0] = v8
+vse32.v v12, x1          # m[x1 = 280] = v8
 C.NOP
 C.NOP
 C.NOP
 C.NOP
 ADDI x1, x1, 16
-vse32.v v16, x1          # m[x1 = 4] = v12
+vse32.v v16, x1          # m[x1 = 290] = v12
 C.NOP
 C.NOP
 C.NOP
