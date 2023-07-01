ec3cdbd2e4a67be1d80e5a1322d7d98fb4ac721a
Celine Joy Capua <130539235+cacapua@users.noreply.github.com>
2023-07-01 12:11:41 +0800

    Update binary-step-no-loop.s

--- Tests/Activation Functions/Using Vector Instructions/Binary Step/binary-step-no-loop.s
+++ Tests/Activation Functions/Using Vector Instructions/Binary Step/binary-step-no-loop.s
@@ -4,316 +4,453 @@ C.NOP
 C.NOP
 vsetivli x20, x5, 10        # 16-bit elements, 512-bit vector
 addi x28, x0, 0             # store data address - load data from m[0]
-addi x9, x0, 28             # max loop counter
-addi x29, x0, 0             # looper
+
 #BINARY STEP
-beq x29, x9, end
 vle32.v v4, x28
 vsrl.vi v8, v4, 15   
+vxor.vi v8, v8, 1
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
-addi x28, x28, 16       # address++
 C.NOP
 C.NOP
 C.NOP
 C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+
 #BINARY STEP
-beq x29, x9, end
 vle32.v v4, x28
 vsrl.vi v8, v4, 15   
+vxor.vi v8, v8, 1
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+
+
 #BINARY STEP
-beq x29, x9, end
 vle32.v v4, x28
 vsrl.vi v8, v4, 15   
+vxor.vi v8, v8, 1
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+
+
 #BINARY STEP
-beq x29, x9, end
 vle32.v v4, x28
 vsrl.vi v8, v4, 15   
+vxor.vi v8, v8, 1
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+
+
 #BINARY STEP
-beq x29, x9, end
 vle32.v v4, x28
 vsrl.vi v8, v4, 15   
+vxor.vi v8, v8, 1
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+
+
 #BINARY STEP
-beq x29, x9, end
 vle32.v v4, x28
 vsrl.vi v8, v4, 15   
+vxor.vi v8, v8, 1
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+
+
 #BINARY STEP
-beq x29, x9, end
 vle32.v v4, x28
 vsrl.vi v8, v4, 15   
+vxor.vi v8, v8, 1
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+
+
 #BINARY STEP
-beq x29, x9, end
 vle32.v v4, x28
 vsrl.vi v8, v4, 15   
+vxor.vi v8, v8, 1
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+
+
 #BINARY STEP
-beq x29, x9, end
 vle32.v v4, x28
 vsrl.vi v8, v4, 15   
+vxor.vi v8, v8, 1
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+
+
 #BINARY STEP
-beq x29, x9, end
 vle32.v v4, x28
 vsrl.vi v8, v4, 15   
+vxor.vi v8, v8, 1
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+
+
 #BINARY STEP
-beq x29, x9, end
 vle32.v v4, x28
 vsrl.vi v8, v4, 15   
+vxor.vi v8, v8, 1
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+
+
 #BINARY STEP
-beq x29, x9, end
 vle32.v v4, x28
 vsrl.vi v8, v4, 15   
+vxor.vi v8, v8, 1
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+
+
 #BINARY STEP
-beq x29, x9, end
 vle32.v v4, x28
 vsrl.vi v8, v4, 15   
+vxor.vi v8, v8, 1
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+
+
 #BINARY STEP
-beq x29, x9, end
 vle32.v v4, x28
 vsrl.vi v8, v4, 15   
+vxor.vi v8, v8, 1
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+
+
 #BINARY STEP
-beq x29, x9, end
 vle32.v v4, x28
 vsrl.vi v8, v4, 15   
+vxor.vi v8, v8, 1
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+
+
 #BINARY STEP
-beq x29, x9, end
 vle32.v v4, x28
 vsrl.vi v8, v4, 15   
+vxor.vi v8, v8, 1
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+
+
 #BINARY STEP
-beq x29, x9, end
 vle32.v v4, x28
 vsrl.vi v8, v4, 15   
+vxor.vi v8, v8, 1
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+
+
 #BINARY STEP
-beq x29, x9, end
 vle32.v v4, x28
 vsrl.vi v8, v4, 15   
+vxor.vi v8, v8, 1
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+
+
 #BINARY STEP
-beq x29, x9, end
 vle32.v v4, x28
 vsrl.vi v8, v4, 15   
+vxor.vi v8, v8, 1
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+
+
 #BINARY STEP
-beq x29, x9, end
 vle32.v v4, x28
 vsrl.vi v8, v4, 15   
+vxor.vi v8, v8, 1
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+
+
 #BINARY STEP
-beq x29, x9, end
 vle32.v v4, x28
 vsrl.vi v8, v4, 15   
+vxor.vi v8, v8, 1
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+
+
 #BINARY STEP
-beq x29, x9, end
 vle32.v v4, x28
 vsrl.vi v8, v4, 15   
+vxor.vi v8, v8, 1
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+
+
 #BINARY STEP
-beq x29, x9, end
 vle32.v v4, x28
 vsrl.vi v8, v4, 15   
+vxor.vi v8, v8, 1
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+
+
 #BINARY STEP
-beq x29, x9, end
 vle32.v v4, x28
 vsrl.vi v8, v4, 15   
+vxor.vi v8, v8, 1
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+
+
 #BINARY STEP
-beq x29, x9, end
 vle32.v v4, x28
 vsrl.vi v8, v4, 15   
+vxor.vi v8, v8, 1
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+
+
 #BINARY STEP
-beq x29, x9, end
 vle32.v v4, x28
 vsrl.vi v8, v4, 15   
+vxor.vi v8, v8, 1
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+
+
 #BINARY STEP
-beq x29, x9, end
 vle32.v v4, x28
 vsrl.vi v8, v4, 15   
+vxor.vi v8, v8, 1
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+
+
 #BINARY STEP
-beq x29, x9, end
 vle32.v v4, x28
 vsrl.vi v8, v4, 15   
+vxor.vi v8, v8, 1
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+
+
 #END
 C.NOP
 C.NOP
