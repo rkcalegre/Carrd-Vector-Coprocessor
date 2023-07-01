87de00d1e81d7eecfea783fafe0168231a6275ef
Celine Joy Capua <130539235+cacapua@users.noreply.github.com>
2023-07-01 15:05:24 +0800

    Update and rename binary-step-stalls.s to binary-step-stalls-no-loop.s

--- Tests/Activation Functions/Using Vector Instructions/Binary Step/binary-step-stalls-no-loop.s
+++ Tests/Activation Functions/Using Vector Instructions/Binary Step/binary-step-stalls-no-loop.s
@@ -4,400 +4,594 @@ C.NOP
 C.NOP
 vsetivli x20, x5, 10        # 16-bit elements, 512-bit vector
 addi x28, x0, 0             # store data address - load data from m[0]
-addi x9, x0, 28             # max loop counter
-addi x29, x0, 0             # looper
+
+
+
 #LOOP
 vle32.v v4, x28
+C.NOP
+C.NOP
 C.NOP
 C.NOP
 vsrl.vi v8, v4, 15  
 C.NOP
 C.NOP
+vxor.vi v8, v8, 1
+C.NOP
+C.NOP
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
 C.NOP
 C.NOP
-C.NOP
-C.NOP
+
 #LOOP
 vle32.v v4, x28
+C.NOP
+C.NOP
 C.NOP
 C.NOP
 vsrl.vi v8, v4, 15  
 C.NOP
 C.NOP
+vxor.vi v8, v8, 1
+C.NOP
+C.NOP
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
 C.NOP
 C.NOP
-C.NOP
-C.NOP
+
 #LOOP
 vle32.v v4, x28
+C.NOP
+C.NOP
 C.NOP
 C.NOP
 vsrl.vi v8, v4, 15  
 C.NOP
 C.NOP
+vxor.vi v8, v8, 1
+C.NOP
+C.NOP
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
 C.NOP
 C.NOP
-C.NOP
-C.NOP
+
 #LOOP
 vle32.v v4, x28
+C.NOP
+C.NOP
 C.NOP
 C.NOP
 vsrl.vi v8, v4, 15  
 C.NOP
 C.NOP
+vxor.vi v8, v8, 1
+C.NOP
+C.NOP
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
 C.NOP
 C.NOP
-C.NOP
-C.NOP
+
 #LOOP
 vle32.v v4, x28
+C.NOP
+C.NOP
 C.NOP
 C.NOP
 vsrl.vi v8, v4, 15  
 C.NOP
 C.NOP
+vxor.vi v8, v8, 1
+C.NOP
+C.NOP
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
 C.NOP
 C.NOP
-C.NOP
-C.NOP
+
 #LOOP
 vle32.v v4, x28
+C.NOP
+C.NOP
 C.NOP
 C.NOP
 vsrl.vi v8, v4, 15  
 C.NOP
 C.NOP
+vxor.vi v8, v8, 1
+C.NOP
+C.NOP
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
 C.NOP
 C.NOP
-C.NOP
-C.NOP
+
 #LOOP
 vle32.v v4, x28
+C.NOP
+C.NOP
 C.NOP
 C.NOP
 vsrl.vi v8, v4, 15  
 C.NOP
 C.NOP
+vxor.vi v8, v8, 1
+C.NOP
+C.NOP
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
 C.NOP
 C.NOP
-C.NOP
-C.NOP
+
 #LOOP
 vle32.v v4, x28
+C.NOP
+C.NOP
 C.NOP
 C.NOP
 vsrl.vi v8, v4, 15  
 C.NOP
 C.NOP
+vxor.vi v8, v8, 1
+C.NOP
+C.NOP
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
 C.NOP
 C.NOP
-C.NOP
-C.NOP
+
 #LOOP
 vle32.v v4, x28
+C.NOP
+C.NOP
 C.NOP
 C.NOP
 vsrl.vi v8, v4, 15  
 C.NOP
 C.NOP
+vxor.vi v8, v8, 1
+C.NOP
+C.NOP
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
 C.NOP
 C.NOP
-C.NOP
-C.NOP
+
 #LOOP
 vle32.v v4, x28
+C.NOP
+C.NOP
 C.NOP
 C.NOP
 vsrl.vi v8, v4, 15  
 C.NOP
 C.NOP
+vxor.vi v8, v8, 1
+C.NOP
+C.NOP
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
 C.NOP
 C.NOP
-C.NOP
-C.NOP
+
 #LOOP
 vle32.v v4, x28
+C.NOP
+C.NOP
 C.NOP
 C.NOP
 vsrl.vi v8, v4, 15  
 C.NOP
 C.NOP
+vxor.vi v8, v8, 1
+C.NOP
+C.NOP
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
 C.NOP
 C.NOP
-C.NOP
-C.NOP
+
 #LOOP
 vle32.v v4, x28
+C.NOP
+C.NOP
 C.NOP
 C.NOP
 vsrl.vi v8, v4, 15  
 C.NOP
 C.NOP
+vxor.vi v8, v8, 1
+C.NOP
+C.NOP
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
 C.NOP
 C.NOP
-C.NOP
-C.NOP
+
 #LOOP
 vle32.v v4, x28
+C.NOP
+C.NOP
 C.NOP
 C.NOP
 vsrl.vi v8, v4, 15  
 C.NOP
 C.NOP
+vxor.vi v8, v8, 1
+C.NOP
+C.NOP
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
 C.NOP
 C.NOP
-C.NOP
-C.NOP
+
 #LOOP
 vle32.v v4, x28
+C.NOP
+C.NOP
 C.NOP
 C.NOP
 vsrl.vi v8, v4, 15  
 C.NOP
 C.NOP
+vxor.vi v8, v8, 1
+C.NOP
+C.NOP
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
 C.NOP
 C.NOP
-C.NOP
-C.NOP
+
 #LOOP
 vle32.v v4, x28
+C.NOP
+C.NOP
 C.NOP
 C.NOP
 vsrl.vi v8, v4, 15  
 C.NOP
 C.NOP
+vxor.vi v8, v8, 1
+C.NOP
+C.NOP
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
 C.NOP
 C.NOP
-C.NOP
-C.NOP
+
 #LOOP
 vle32.v v4, x28
+C.NOP
+C.NOP
 C.NOP
 C.NOP
 vsrl.vi v8, v4, 15  
 C.NOP
 C.NOP
+vxor.vi v8, v8, 1
+C.NOP
+C.NOP
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
 C.NOP
 C.NOP
-C.NOP
-C.NOP
+
 #LOOP
 vle32.v v4, x28
+C.NOP
+C.NOP
 C.NOP
 C.NOP
 vsrl.vi v8, v4, 15  
 C.NOP
 C.NOP
+vxor.vi v8, v8, 1
+C.NOP
+C.NOP
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
 C.NOP
 C.NOP
-C.NOP
-C.NOP
+
 #LOOP
 vle32.v v4, x28
+C.NOP
+C.NOP
 C.NOP
 C.NOP
 vsrl.vi v8, v4, 15  
 C.NOP
 C.NOP
+vxor.vi v8, v8, 1
+C.NOP
+C.NOP
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
 C.NOP
 C.NOP
-C.NOP
-C.NOP
+
 #LOOP
 vle32.v v4, x28
+C.NOP
+C.NOP
 C.NOP
 C.NOP
 vsrl.vi v8, v4, 15  
 C.NOP
 C.NOP
+vxor.vi v8, v8, 1
+C.NOP
+C.NOP
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
 C.NOP
 C.NOP
-C.NOP
-C.NOP
+
 #LOOP
 vle32.v v4, x28
+C.NOP
+C.NOP
 C.NOP
 C.NOP
 vsrl.vi v8, v4, 15  
 C.NOP
 C.NOP
+vxor.vi v8, v8, 1
+C.NOP
+C.NOP
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
 C.NOP
 C.NOP
-C.NOP
-C.NOP
+
 #LOOP
 vle32.v v4, x28
+C.NOP
+C.NOP
 C.NOP
 C.NOP
 vsrl.vi v8, v4, 15  
 C.NOP
 C.NOP
+vxor.vi v8, v8, 1
+C.NOP
+C.NOP
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
 C.NOP
 C.NOP
-C.NOP
-C.NOP
+
 #LOOP
 vle32.v v4, x28
+C.NOP
+C.NOP
 C.NOP
 C.NOP
 vsrl.vi v8, v4, 15  
 C.NOP
 C.NOP
+vxor.vi v8, v8, 1
+C.NOP
+C.NOP
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
 C.NOP
 C.NOP
-C.NOP
-C.NOP
+
 #LOOP
 vle32.v v4, x28
+C.NOP
+C.NOP
 C.NOP
 C.NOP
 vsrl.vi v8, v4, 15  
 C.NOP
 C.NOP
+vxor.vi v8, v8, 1
+C.NOP
+C.NOP
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
 C.NOP
 C.NOP
-C.NOP
-C.NOP
+
 #LOOP
 vle32.v v4, x28
+C.NOP
+C.NOP
 C.NOP
 C.NOP
 vsrl.vi v8, v4, 15  
 C.NOP
 C.NOP
+vxor.vi v8, v8, 1
+C.NOP
+C.NOP
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
 C.NOP
 C.NOP
-C.NOP
-C.NOP
+
 #LOOP
 vle32.v v4, x28
+C.NOP
+C.NOP
 C.NOP
 C.NOP
 vsrl.vi v8, v4, 15  
 C.NOP
 C.NOP
+vxor.vi v8, v8, 1
+C.NOP
+C.NOP
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
 C.NOP
 C.NOP
-C.NOP
-C.NOP
+
 #LOOP
 vle32.v v4, x28
+C.NOP
+C.NOP
 C.NOP
 C.NOP
 vsrl.vi v8, v4, 15  
 C.NOP
 C.NOP
+vxor.vi v8, v8, 1
+C.NOP
+C.NOP
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
 C.NOP
 C.NOP
-C.NOP
-C.NOP
+
 #LOOP
 vle32.v v4, x28
+C.NOP
+C.NOP
 C.NOP
 C.NOP
 vsrl.vi v8, v4, 15  
 C.NOP
 C.NOP
+vxor.vi v8, v8, 1
+C.NOP
+C.NOP
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
+C.NOP
+C.NOP
+C.NOP
+C.NOP
 addi x28, x28, 16       # address++
 C.NOP
 C.NOP
-C.NOP
-C.NOP
+
 #LOOP
 vle32.v v4, x28
+C.NOP
+C.NOP
 C.NOP
 C.NOP
 vsrl.vi v8, v4, 15  
 C.NOP
 C.NOP
+vxor.vi v8, v8, 1
+C.NOP
+C.NOP
 vse32.v v8, x28
-addi x29, x29, 1        # loop_iter++
-addi x28, x28, 16       # address++
 C.NOP
 C.NOP
 C.NOP
 C.NOP
+
 #END
 C.NOP
 C.NOP
@@ -416,4 +610,3 @@ C.NOP
 C.NOP
 C.NOP
 C.NOP
-
