5f5ab7431e956b52b3fe143178f63fb8214c3196
Dimple Mae Manalo <130539054+dnmanalo@users.noreply.github.com>
2023-07-02 06:48:59 +0800

    updated and working

--- Tests/Activation Functions/Using Scalar Instructions/Binary Step/binary-step.s
+++ Tests/Activation Functions/Using Scalar Instructions/Binary Step/binary-step.s
@@ -1,21 +1,24 @@
-addi x5, x0, 0              # store data address - load data from m[0]
-addi x6, x0, 0              # answer comparer
-addi x7, x0, 0              # looper
-addi x30, x0, 3         
-slli x30, x30, 4
-addi x30, x30, 1
-slli x30, x30, 4            # max loop = 784 = 28*28 dataset
-addi x28, x0, 15            # value for a
+ADDI x8, x0, 0              # store data address - load data from m[0]
+ADDI x9, x0, 1              # if greater than or equal to zero
+ADDI x30, x0, 3         
+SLLI x30, x30, 4
+ADDI x30, x30, 1
+SLLI x30, x30, 6           # max loop = 784 (28*28 dataset) * 4 (stalls for load/store)
 jal, x0, binary_step
 
-binary_step:                # f(x) = 0 if x < 0, 1 if x > 0
-    beq x7, x30, end
-    lw x18, 0(x5)
-    slt x19, x18, x6        # R[rd] = (rs1 < rs2) ? 1 : 0
-    addi x7, x7, 1
-    sw x19, 0(x5)
-    addi x5, x5, 1          # data_address++ 
+
+binary_step:                # f(x) = max(x, 0)
+    BEQ x8, x30, end
+    LW x18, 0(x8)
+    BLT x18, x0, zero
+    SW x9, 0(x8)
+    ADDI x8, x8, 1          # data_address++ 
     jal, x0, binary_step
+
+zero:
+    SW x0, 0(x8)
+    ADDI x8, x8, 1          # data_address++ 
+    jal, x0, zero
 
 end:
     C.NOP
@@ -35,4 +38,3 @@ end:
     C.NOP
     C.NOP
     C.NOP
-
