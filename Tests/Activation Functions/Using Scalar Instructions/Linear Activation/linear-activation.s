e12eb470f77a55f1abece62296221c26bc5f11c0
Dimple Mae Manalo <130539054+dnmanalo@users.noreply.github.com>
2023-07-02 03:59:33 +0800

    Update linear-activation.s

--- Tests/Activation Functions/Using Scalar Instructions/Linear Activation/linear-activation.s
+++ Tests/Activation Functions/Using Scalar Instructions/Linear Activation/linear-activation.s
@@ -1,19 +1,20 @@
-addi x5, x0, 0              # store data address - load data from m[0]
-addi x6, x0, 0              # answer comparer
-addi x7, x0, 0              # looper
-addi x30, x0, 3         
-slli x30, x30, 4
-addi x30, x30, 1
-slli x30, x30, 4            # max loop = 784 = 28*28 dataset
-addi x28, x0, 15            # value for a
+ADDI x5, x0, 0              # store data address - load data from m[0]
+ADDI x6, x0, 0              # anSWer comparer
+ADDI x7, x0, 0              # looper
+ADDI x30, x0, 3         
+SLLI x30, x30, 4
+ADDI x30, x30, 1
+SLLI x30, x30, 4            # max loop = 784 = 28*28 dataset
+ADDI x28, x0, 15            # value for a
 jal, x0, linear_activation
 
 linear_activation:          # f(x) = x
-    lw x18, 0(x5)
-    addi x19, x18, 0
-    addi x7, x7, 1
-    sw x19, 0(x5)
-    addi x5, x5, 1          # data_address++ 
+    BEQ x7, x30, end
+    LH x27, 0(x5)
+    ADDI x19, x27, 0
+    SH x19, 0(x5)
+    ADDI x7, x7, 1
+    ADDI x5, x5, 1          # data_address++ 
     jal, x0, linear_activation
 
 end:
