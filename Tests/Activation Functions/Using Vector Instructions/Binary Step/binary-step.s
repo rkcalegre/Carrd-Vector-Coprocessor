97a08281023b7d464490588504d14656d4ce5e83
Celine Joy Capua <130539235+cacapua@users.noreply.github.com>
2023-07-01 23:23:05 +0800

    binary step with loop

--- Tests/Activation Functions/Using Vector Instructions/Binary Step/binary-step.s
+++ Tests/Activation Functions/Using Vector Instructions/Binary Step/binary-step.s
@@ -1,25 +1,31 @@
-addi x5, x0, 16             # x5 = 16
-C.NOP
-C.NOP
-C.NOP
-vsetivli x20, x5, 10        # 16-bit elements, 512-bit vector
-addi x28, x0, 0             # store data address - load data from m[0]
-addi x9, x0, 28             # max loop counter
-addi x29, x0, 0             # looper
+addi x5, x0, 16                 # x5 = 16
+addi x28, x0, 0                 # store data address - load data from m[0]
+addi x9, x0, 28                  # max loop counter
+addi x29, x0, 0                 # looper
+vsetivli x20, x5, 10            # 16-bit elements, 512-bit vector
 jal, x0, binary_step
 
-binary_step:                # f(x) = 0 if x < 0, 1 if x > 0
+
+binary_step:              # f(x) = x
+    addi x0, x0, 0
     beq x29, x9, end
     vle32.v v4, x28
-    vsrl.vi v8, v4, 15   
+    addi x0, x0, 0
+    addi x0, x0, 0
+    addi x0, x0, 0
+    addi x0, x0, 0
+    addi x0, x0, 0
+    vsrl.vi v8, v4, 15
+    addi x0, x0, 0
+    addi x0, x0, 0
+    vxor.vi v8, v8, 1
+    addi x0, x0, 0
+    addi x0, x0, 0
     vse32.v v8, x28
-    addi x29, x29, 1        # loop_iter++
-    addi x28, x28, 16       # address++
-    C.NOP
-    C.NOP
-    C.NOP
-    C.NOP
+    addi x29, x29, 1            # loop_iter++
+    addi x28, x28, 16           # address++
     jal, x0, binary_step
+
 
 end:
     C.NOP
@@ -39,4 +45,3 @@ end:
     C.NOP
     C.NOP
     C.NOP
-
