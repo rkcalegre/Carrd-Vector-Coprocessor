524c1e870f08d845d1cff74425b73c5fdc216b11
Celine Joy Capua <130539235+cacapua@users.noreply.github.com>
2023-07-07 14:49:57 +0800

    ports matched

--- CARRD-With-Optimizations/v_mul.sv
+++ CARRD-With-Optimizations/v_mul.sv
@@ -7,29 +7,29 @@ module v_mul
 	input nrst,
     input logic [31:0] op_A,
     input logic [31:0] op_B,
-	input logic [1:0] sew,
+	input logic [2:0] sew,
 	input logic is_mul,
 	output logic [31:0] result //change based on meet
 );
 
 
-	logic [63:0] mult_32_0_out;
-	logic [31:0] mult_16_0_out;
-	logic [15:0] mult_8_0_out;
-	logic [15:0] mult_8_1_out;
+	logic [31:0] mult_32_0_out;
+	logic [15:0] mult_16_0_out;
+	logic [7:0] mult_8_0_out;
+	logic [7:0] mult_8_1_out;
 
 	assign mult_32_0 = 1'b1;
-	assign mult_16_0 = (sew == 2'b00 || sew == 2'b01) ? 1'b1:1'b0;
-	assign mult_8_0 = (sew==2'b00) ? 1'b1:1'b0;
-	assign mult_8_1 = (sew==2'b00) ? 1'b1:1'b0;
+	assign mult_16_0 = (sew == 3'b000 || sew == 2'b001) ? 1'b1:1'b0;
+	assign mult_8_0 = (sew==3'b000) ? 1'b1:1'b0;
+	assign mult_8_1 = (sew==3'b000) ? 1'b1:1'b0;
 
 
 
 	mult_gen_32 mult_gen_32_00(
 		.CLK(clk),
 		.CE(mult_32_0),
-		.A((sew==2'b00) ? {{24{op_A[7]}},{op_A[7:0]}}: ((sew==2'b01) ? {{16{op_A[15]}},{op_A[15:0]}}:(op_A[31:0]))),
-		.B(((sew==2'b00) ? {{24{op_B[7]}},{op_B[7:0]}}: ((sew==2'b01) ? {{16{op_B[15]}},{op_B[15:0]}}:(op_B[31:0])))),
+		.A((sew==3'b000) ? {{24{op_A[7]}},{op_A[7:0]}}: ((sew==3'b001) ? {{16{op_A[15]}},{op_A[15:0]}}:(op_A[31:0]))),
+		.B(((sew==3'b000) ? {{24{op_B[7]}},{op_B[7:0]}}: ((sew==3'b001) ? {{16{op_B[15]}},{op_B[15:0]}}:(op_B[31:0])))),
 		.P(mult_32_0_out)
 	);
 
@@ -39,8 +39,8 @@ module v_mul
 	mult_gen_16 mult_gen_16_00(
 		.CLK(clk),
 		.CE(mult_16_0),
-		.A((sew==2'b00) ? {{8{op_A[15]}},{op_A[15:8]}}: op_A[31:16]),
-		.B(((sew==2'b00) ? {{8{op_B[15]}},{op_B[15:8]}}: op_B[31:16])),
+		.A((sew==3'b000) ? {{8{op_A[15]}},{op_A[15:8]}}: op_A[31:16]),
+		.B(((sew==3'b000) ? {{8{op_B[15]}},{op_B[15:8]}}: op_B[31:16])),
 		.P(mult_16_0_out)
 	);
 	
@@ -65,11 +65,11 @@ module v_mul
 		case (is_mul)
 			1'b1: begin //VMUL
 				case (sew)
-					2'b00:
+					3'b000:
 						result = {mult_8_1_out [7:0], mult_8_0_out [7:0], mult_16_0_out [7:0], mult_32_0_out[7:0]};
-					2'b01:
+					3'b001:
 						result = {mult_16_0_out [15:0],mult_32_0_out[15:0]};
-					2'b10:
+					3'b010:
 						result = mult_32_0_out[31:0];
 					default:
 						result = 0;
