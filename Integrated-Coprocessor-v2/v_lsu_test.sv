4dc119245866afb5f13b1b212e2dc3700f6a5420
Dimple Mae Manalo <130539054+dnmanalo@users.noreply.github.com>
2023-07-11 11:29:48 +0800

    tracking vlsu changes pt. 2

--- Integrated-Coprocessor-v2/v_lsu_test.sv
+++ Integrated-Coprocessor-v2/v_lsu_test.sv
@@ -48,7 +48,7 @@ module v_lsu #(
     output logic [`DATAMEM_WIDTH-1:0] data_out3,
     output logic dm_v_write,
     output logic s_done,
-    output logic [6:0] out_ctr
+    output logic [6:0] out_ctr2
 );
 
     import v_pkg::*;
@@ -59,7 +59,7 @@ module v_lsu #(
     logic [4:0] iter;                       // ensures all of the elements in each register are stored
     logic [4:0] exe_cc;                     // # of clock cycles per operation
     wire [1:0] s_cc;
-    logic [`DATAMEM_BITS-1:0] temp_addr;
+    logic [`DATAMEM_BITS-1:0] temp_addr, temp_addr_load;
     logic [6:0] strided_cc;
     logic nrst_ctr;
 
@@ -68,25 +68,28 @@ module v_lsu #(
     assign iter = (elem_per_vreg == 5'd16) ? 5'd4 : (elem_per_vreg == 5'd8) ? 5'd2 : (elem_per_vreg == 5'd4) ? 5'd1 : 5'd1;
     assign strided_cc = (v_lsu_op == 4'd10) ? 7'd4 : (v_lsu_op == 4'd11) ? 7'd2 : (v_lsu_op == 4'd12) ? 7'd1 : 7'd4; 
     assign exe_cc = (v_lsu_op inside {[7:9]}) ? num_reg : (v_lsu_op inside {[10:12]}) ? ((stride inside {2,6,10,14}) ? num_reg*2*strided_cc : (stride inside {3,5,7,9,11,13,15}) ? num_reg*1*strided_cc : (stride inside {4,8,12,16}) ? num_reg*4*strided_cc : 0) : 1;
+    //assign s_tmp_done = (v_lsu_op inside {[7:12]} && s_cc == exe_cc-1) ? 1 : 0;
     assign dm_v_write = (v_lsu_op inside {[7:12]}) ? 1 : 0;
 
     // load signals
     logic [511:0] temp_data;
     logic [511:0] loaddata;
-	logic [2:0] max_reg;	
-	logic [2:0] l_cc = 0;
+    logic [4:0] max_cc; 	
+	logic [2:0] l_cc;
 	logic [127:0] temp;
 	logic [511:0] hold;
     logic [5:0] l_stride_cc = 0;
     logic is_vltype;
 
-	assign max_reg = (lmul == 3'b000) ? 3'd1 : (lmul == 3'b001) ? 3'd2 : (lmul == 3'b010) ? 3'd4 : 3'd1;  
+    assign max_cc = (v_lsu_op inside {[1:3]}) ? num_reg : (v_lsu_op inside {[4:6]}) ? (((stride == 6'b0) || (stride == 6'b1)) ? num_reg :(stride[1:0] == 2'd0) ? elem_per_vreg*num_reg : (stride[1:0] == 2'd1) ? num_reg*num_reg : (stride[1:0] == 2'd2) ? num_reg*num_reg*2 : num_reg*num_reg):  0;
+    assign l_done = (l_cc == exe_cc + 2)? 1: 0;
     assign is_vltype = (v_lsu_op inside {[1:6]});
-    
+    assign temp_addr_load = address + (l_cc* 4);
+    assign out_ctr2 = l_cc;
 
     // assign temp signals for store
     assign s_cc = out_ctr;
-    assign nrst_ctr = (v_lsu_op inside {[7:12]}) ? 1 : 0;
+    assign nrst_ctr = (v_lsu_op inside {[7:9]}) ? 1 : 0;
 
     // instantiate counters
     counter c4 (
@@ -94,6 +97,13 @@ module v_lsu #(
         .nrst(nrst_ctr),
         .out(out_ctr)
     );
+
+    // instantiate clock cycle counter for load
+    counter_load ctr_load (
+        .clk(clk && is_vltype),
+        .nrst(nrst && is_vltype),
+        .out(l_cc)
+    );    
 
     // set done signal
     always @(posedge clk) begin
@@ -104,6 +114,14 @@ module v_lsu #(
                 s_done <= 1;
             end else
                 s_done <= 0;
+        end
+        if (!nrst) begin
+            l_data_out <= 0;
+        end else begin
+            if (l_cc == exe_cc + 1) begin
+                l_data_out <= (lmul == 3'b010)? loaddata: (lmul==3'b001) ? {{256{1'b0}},{loaddata[255:0]}}: {{384{1'b0}},{loaddata[127:0]}};
+            end else
+            l_data_out <= 0 ;
         end
     end
 
@@ -176,9 +194,9 @@ module v_lsu #(
                             temp_data <= 0;
                         end
                     endcase
-                end
+                end            
             end
-        end 
+        end
     end
 
     // set value of output addresses
@@ -214,6 +232,49 @@ module v_lsu #(
                     data_addr2 = (s_cc != exe_cc + 1) ? temp_addr + stride*2 : data_addr2;
                     data_addr3 = (s_cc != exe_cc + 1) ? temp_addr + stride*3 : data_addr3;
                 end
+                default: begin
+                    data_addr0 = 0;
+                    data_addr1 = 0;
+                    data_addr2 = 0;
+                    data_addr3 = 0;
+                end
+            endcase
+        end else if (v_lsu_op inside {[1:3]}) begin             
+//            temp_addr = (l_cc == 0) ? address : temp_addr + 4;
+            data_addr0 = (l_cc <= max_cc) ? temp_addr_load : data_addr0;
+            data_addr1 = (l_cc <= max_cc) ? temp_addr_load + 1 : data_addr1;
+            data_addr2 = (l_cc <= max_cc) ? temp_addr_load + 2 : data_addr2;
+            data_addr3 = (l_cc <= max_cc) ? temp_addr_load + 3 : data_addr3;    
+        end else if (v_lsu_op inside {[4:6]}) begin
+            case (stride[1:0])
+                //4,8,12,16: begin
+                2'd0: begin
+                    data_addr0 = (l_cc != max_cc) ? temp_addr_load : data_addr0;
+                    data_addr1 = (l_cc != max_cc) ? 0 : 0;
+                    data_addr2 = (l_cc != max_cc) ? 0 : 0;
+                    data_addr3 = (l_cc != max_cc) ? 0 : 0;
+                end
+                //5,9,13: begin
+                2'd1: begin
+                    data_addr0 = (l_cc != max_cc) ? temp_addr_load : data_addr0;
+                    data_addr1 = (l_cc != max_cc) ? temp_addr_load + stride : data_addr1;
+                    data_addr2 = (l_cc != max_cc) ? temp_addr_load + stride*2 : data_addr2;
+                    data_addr3 = (l_cc != max_cc) ? temp_addr_load + stride*3 : data_addr3;
+                end
+                //2,6,10,14: begin
+                2'd2: begin
+                    data_addr0 = (l_cc != max_cc) ? temp_addr_load : data_addr0;
+                    data_addr1 = (l_cc != max_cc) ? 0 : 0;
+                    data_addr2 = (l_cc != max_cc) ? temp_addr_load + stride : data_addr2;
+                    data_addr3 = (l_cc != max_cc) ? 0 : 0;
+                end
+                //3,7,11,15: begin
+                2'd3: begin
+                    data_addr0 = (l_cc != max_cc) ? temp_addr_load : data_addr0;
+                    data_addr1 = (l_cc != max_cc) ? temp_addr_load + stride*3 : data_addr1;
+                    data_addr2 = (l_cc != max_cc) ? temp_addr_load + stride*2 : data_addr2;
+                    data_addr3 = (l_cc != max_cc) ? temp_addr_load + stride : data_addr3;  
+                end            
                 default: begin
                     data_addr0 = 0;
                     data_addr1 = 0;
@@ -348,6 +409,15 @@ module v_lsu #(
                     end  
                 endcase
             end
+            VLSU_VLE8, VLSU_VLE16, VLSU_VLE32: begin
+                case(l_cc)
+                    7'd1: loaddata[127:0] = {{l_data_in3},{l_data_in2},{l_data_in1},{l_data_in0}};
+                    7'd2: loaddata[255:128] = {{l_data_in3},{l_data_in2},{l_data_in1},{l_data_in0}};
+                    7'd3: loaddata[383:256] = {{l_data_in3},{l_data_in2},{l_data_in1},{l_data_in0}};
+                    7'd4: loaddata[511:384] = {{l_data_in3},{l_data_in2},{l_data_in1},{l_data_in0}};
+                    default: loaddata[127:0] = {{l_data_in3},{l_data_in2},{l_data_in1},{l_data_in0}};
+                endcase
+            end            
             default: begin
                 storedata = 0;
                 data_out0 = 0;
@@ -371,6 +441,24 @@ module counter (
         out <= 0;
     end
     always @(posedge clk) begin
+        if (!nrst)
+            out <= 0;
+        else    
+            out <= out + 1;
+    end
+    
+endmodule
+
+// 7-bit Counter
+module counter_load (
+    input clk, 
+    input nrst,
+    output reg [6:0] out
+);
+    initial begin
+        out <= 0;
+    end
+    always @(posedge clk or negedge nrst) begin
         if (!nrst)
             out <= 0;
         else    
