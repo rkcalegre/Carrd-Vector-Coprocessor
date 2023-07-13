08d0fe5bc72d1fee54035650b83709d504c62403
Celine Joy Capua <130539235+cacapua@users.noreply.github.com>
2023-07-13 13:49:34 +0800

    Update vvadd.s

--- Tests/benchmark/vvadd/vvadd.s
+++ Tests/benchmark/vvadd/vvadd.s
@@ -1,10 +1,8 @@
 ADDI x5, x0, 16         # x5 = 16
-vsetivli x20, x5, 18    # 32 bit elements 512-bit vector
 ADDI x1, x0, 16
-ADDI x2, x0, 32
-addi x0, x0, 0
-addi x0, x0, 0
-addi x0, x0, 0
+ADDI x2, x0, 16
+ADDI x2, x2, 16
+vsetivli x20, x5, 18    # 32 bit elements 512-bit vector
 vle32.v v0, x0
 addi x0, x0, 0
 addi x0, x0, 0
@@ -13,7 +11,7 @@ addi x0, x0, 0
 addi x0, x0, 0
 addi x0, x0, 0
 addi x0, x0, 0
-vle32.v v4, x1
+vle32.v v4, x5
 addi x0, x0, 0
 addi x0, x0, 0
 addi x0, x0, 0
@@ -22,8 +20,6 @@ addi x0, x0, 0
 addi x0, x0, 0
 addi x0, x0, 0
 vadd.vv v8, v0, v4
-addi x0, x0, 0
-addi x0, x0, 0
 addi x0, x0, 0
 addi x0, x0, 0
 addi x0, x0, 0
@@ -35,12 +31,7 @@ addi x0, x0, 0
 addi x0, x0, 0
 addi x0, x0, 0
 vsetivli x20, x5, 10    # 16 bit elements 512-bit vector
-addi x0, x0, 0
-addi x0, x0, 0
 vadd.vv v8, v0, v4
-ADDI x1, x0, 16
-addi x0, x0, 0
-addi x0, x0, 0
 addi x0, x0, 0
 addi x0, x0, 0
 addi x0, x0, 0
@@ -52,17 +43,11 @@ addi x0, x0, 0
 addi x0, x0, 0
 addi x0, x0, 0
 vsetivli x20, x5, 2     # 8 bit elements 512-bit vector
-addi x0, x0, 0
-addi x0, x0, 0
 vadd.vv v8, v0, v4
-ADDI x2, x0, 32
-addi x0, x0, 0
-addi x0, x0, 0
 addi x0, x0, 0
 addi x0, x0, 0
 addi x0, x0, 0
 vse32.v v8, x2
-ADDI x5, x0, 80
 addi x0, x0, 0
 addi x0, x0, 0
 addi x0, x0, 0
