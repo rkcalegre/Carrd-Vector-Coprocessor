58cac5aa5e6800217660508901421e111da473bd
Celine Joy Capua <130539235+cacapua@users.noreply.github.com>
2023-07-13 14:46:30 +0800

    Update dotproduct-with-stalls.s

--- Tests/benchmark/dotproduct/with stalls - random 512b/dotproduct-with-stalls.s
+++ Tests/benchmark/dotproduct/with stalls - random 512b/dotproduct-with-stalls.s
@@ -1,119 +1,87 @@
 ADDI x5, x0, 16         # x5 = 16
+ADDI x1, x0, 16
+ADDI x2, x0, 32
 vsetivli x20, x5, 18    # 32 bit elements 512-bit vector
-ADDI x1, x0, 16
-ADDI x2, x0, 32
-C.NOP
-C.NOP
-C.NOP
 vle32.v v0, x0
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
 vle32.v v4, x1
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
 vmul.vv v8, v0, v4
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
 vredsum.vs v12, v8, v28   #v28 is a zero vector
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-vse32.v v8, x0
-ADDI x1, x0, 16
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-vse32.v v12, x1
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+vse32.v v12, x0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
 vsetivli x20, x5, 10    # 16 bit elements 512-bit vector
-C.NOP
-C.NOP
 vmul.vv v8, v0, v4
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
 vredsum.vs v12, v8, v28   #v28 is a zero vector
-ADDI x2, x0, 32
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-vse32.v v8, x2
-ADDI x3, x0, 48
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-vse32.v v12, x3
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+vse32.v v12, x1
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
 vsetivli x20, x5, 2     # 8 bit elements 512-bit vector
-C.NOP
-C.NOP
 vmul.vv v8, v0, v4
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
 vredsum.vs v12, v8, v28   #v28 is a zero vector
-ADDI x4, x0,64
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-vse32.v v8, x4
-ADDI x5, x0, 80
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-vse32.v v12, x5
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+vse32.v v12, x2
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
