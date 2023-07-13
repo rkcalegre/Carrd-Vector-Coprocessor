1056a294429c069c757bbe7f75b4fb825ef6a20a
rkcalegre <raine.alegre@eee.upd.edu.ph>
2023-07-14 03:32:50 +0800

    updated v_mul

--- Timing/v_mul.sv
+++ Timing/v_mul.sv
@@ -1,64 +1,84 @@
 `timescale 1ns / 1ps
 
-
 module v_mul
 (
 	
 	input clk,
 	input nrst,
+	input is_mul,
     input logic [31:0] op_A,
     input logic [31:0] op_B,
-	input logic [2:0] sew,
-	input logic is_mul,
+	input logic [1:0] sew,
+	input logic [5:0] op_instr,
 	output logic [31:0] result //change based on meet
 );
 
 
-	logic [31:0] mult_32_0_out;
-	logic [15:0] mult_16_0_out;
+	logic [63:0] mult_32_0_out;
+	logic [31:0] result2;
+	logic [31:0] result1;
+	logic [31:0] mult_16_0_out;
+	logic [31:0] mult_16_1_out;
+	logic [31:0] mult_16_2_out;
 	logic [7:0] mult_8_0_out;
 	logic [7:0] mult_8_1_out;
-    logic [31:0] opA_32;
-    logic [31:0] opB_32;
+	logic [31:0] op_B_u;
+	logic [31:0] op_A_u;
+	logic sign;
 
 	assign mult_32_0 = 1'b1;
-	assign mult_16_0 = (sew == 3'b000 || sew == 2'b001) ? 1'b1:1'b0;
-	assign mult_8_0 = (sew == 3'b000) ? 1'b1:1'b0;
-	assign mult_8_1 = (sew == 3'b000) ? 1'b1:1'b0;
+	assign mult_32_2 = sew == 2'b10 ? 1'b1: 1'b0;
+	assign mult_8_0 = (sew==2'b00) ? 1'b1:1'b0;
+	assign mult_8_1 = (sew==2'b00) ? 1'b1:1'b0;
+	assign sign = op_A[31] ^ op_B[31];
+
+	assign op_A_u = ({32{op_A[31]}} ^ op_A) + op_A[31];
+	assign op_B_u = ({32{op_B[31]}} ^ op_B) + op_B[31];
 
 
-    always_comb begin
-        case (sew)  
-            3'b000: begin
-                opA_32 = {{24{op_A[7]}},{op_A[7:0]}};
-                opB_32 =  {{24{op_B[7]}},{op_B[7:0]}};
-            end
-            3'b001: begin
-                opA_32 = {{16{op_A[15]}},{op_A[15:0]}};
-                opB_32 =  {{16{op_B[15]}},{op_B[15:0]}};
-            end
-            default: begin
-                opA_32 = op_A;
-                opB_32 = op_B;
-            end
-        endcase
-    end
-	mult_gen_32 mult_gen_32_00(
+
+
+	mult_gen_16 mult_gen_16_00(
 		.CLK(clk),
 		.CE(mult_32_0),
-		.A(opA_32),
-		.B(opB_32),
-		.P(mult_32_0_out)
-	);
+		.A((sew==2'b00) ? {{8{op_A[7]}},{op_A[7:0]}}: (sew == 2'b01) ? op_A[15:0]: op_A_u[15:0]),
+		.B((sew==2'b00) ? {{8{op_B[7]}},{op_B[7:0]}}: (sew == 2'b01) ? op_B[15:0]: op_B_u[15:0]),
+		.P(mult_16_0_out)
+	); 	
 	///////
+ 	mult_gen_16 mult_gen_16_01(
+		.CLK(clk),
+		.CE(mult_32_0),
+		.A((sew==2'b00) ? {{8{op_A[15]}},{op_A[15:8]}}: (sew == 2'b01) ? op_A[31:16]: op_A_u[31:16]),
+		.B((sew==2'b00) ? {{8{op_B[15]}},{op_B[15:8]}}: (sew == 2'b01) ? op_B[31:16]: op_B_u[15:0]),
+		.P(mult_16_1_out)
+	);
+
+	mult_gen_16 mult_gen_16_02(
+		.CLK(clk),
+		.CE(mult_32_2),
+		.A(op_B_u[31:16]),
+		.B(op_A_u[15:0]),
+		.P(mult_16_2_out)
+	);
+/* 	
+ 	mult_gen_32 mult_gen_32_00(
+		.CLK(clk),
+		.CE(mult_32_0),
+		.A((sew==2'b00) ? {{24{op_A[7]}},{op_A[7:0]}}: ((sew==2'b01) ? {{16{op_A[15]}},{op_A[15:0]}}:(op_A[31:0]))),
+		.B(((sew==2'b00) ? {{24{op_B[7]}},{op_B[7:0]}}: ((sew==2'b01) ? {{16{op_B[15]}},{op_B[15:0]}}:(op_B[31:0])))),
+		.P(mult_32_0_out)
+	); 
 	mult_gen_16 mult_gen_16_00(
 		.CLK(clk),
 		.CE(mult_16_0),
-		.A((sew==3'b000) ? {{8{op_A[15]}},{op_A[15:8]}}: op_A[31:16]),
-		.B(((sew==3'b000) ? {{8{op_B[15]}},{op_B[15:8]}}: op_B[31:16])),
+		.A((sew==2'b00) ? {{8{op_A[15]}},{op_A[15:8]}}: op_A[31:16]),
+		.B((sew==2'b00) ? {{8{op_B[15]}},{op_B[15:8]}}: op_B[31:16]),
 		.P(mult_16_0_out)
-	);
+	); */
+	
 	/// 
+	
 	mult_gen_8 mult_gen_8_00(
 		.CLK(clk),
 		.CE(mult_8_0),
@@ -75,22 +95,27 @@ module v_mul
 	);
 	
 	always_comb begin 
-		case (is_mul)
-			1'b1: begin //VMUL
-				case (sew)
-					3'b000:
-						result = {mult_8_1_out [7:0], mult_8_0_out [7:0], mult_16_0_out [7:0], mult_32_0_out[7:0]};
-					3'b001:
-						result = {mult_16_0_out [15:0],mult_32_0_out[15:0]};
-					3'b010:
-						result = mult_32_0_out[31:0];
-					default:
-						result = 0;
-				endcase
-			end
-			default: result = 0;
-		endcase
+        if (is_mul == 1) begin
+            case (sew)
+                2'b00:
+                    //result = {mult_8_1_out [7:0], mult_8_0_out [7:0], mult_16_0_out [7:0], mult_32_0_out[7:0]};
+                    result = {mult_8_1_out [7:0], mult_8_0_out [7:0], mult_16_1_out [7:0], mult_16_0_out[7:0]};
+                2'b01:
+                    //result = {mult_16_0_out[15:0],mult_32_0_out[15:0]};
+                    result = {mult_16_1_out[15:0],mult_16_0_out[15:0]};
+                2'b10: begin
+                    //result = mult_32_0_out;
+                    //result = mult_16_0_out + mult_16_1_out;
+                    result1 = mult_16_1_out + mult_16_2_out;
+                    result2 = {{result1[15:0]}, {16{1'b0}}}  + mult_16_0_out;
+                    //result = result2 + mult_16_0_out;
+                    result = ({32{sign}} ^ result2) + sign;
+                end
+                default:
+                    result = 0;
+            endcase      
+        end else begin
+            result = 0;
+        end
 	end
-
-
 endmodule
