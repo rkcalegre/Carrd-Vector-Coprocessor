f20ba29e760969e3b98eba3c317e87eda967a7a1
Celine Joy Capua <130539235+cacapua@users.noreply.github.com>
2023-07-13 14:03:13 +0800

    Update multiplication.s

--- Tests/benchmark/multiplication/multiplication.s
+++ Tests/benchmark/multiplication/multiplication.s
@@ -1,12 +1,66 @@
 ADDI x5, x0, 16         # x5 = 16
-vsetivli x20, x5, 16    # 32 bit elements 128-bit vector
 ADDI x1, x0, 16
-ADDI x2, x0, 32
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
+ADDI x2, x0, 16
+ADDI x2, x2, 16
+vsetivli x20, x5, 18    # 32 bit elements 512-bit vector
 vle32.v v0, x0
-vle32.v v4, x1
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+vle32.v v4, x5
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
 vmul.vv v8, v0, v4
-ADDI x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
 vse32.v v8, x0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+vsetivli x20, x5, 10    # 16 bit elements 512-bit vector
+vmul.vv v8, v0, v4
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+vse32.v v8, x1
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+vsetivli x20, x5, 2     # 8 bit elements 512-bit vector
+vmul.vv v8, v0, v4
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+vse32.v v8, x2
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
