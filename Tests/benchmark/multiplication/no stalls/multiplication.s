1554bcdbca8128ce84224fe6f886eeedb09af982
Celine Joy Capua <130539235+cacapua@users.noreply.github.com>
2023-07-13 14:01:33 +0800

    Update multiplication.s

--- Tests/benchmark/multiplication/no stalls/multiplication.s
+++ Tests/benchmark/multiplication/no stalls/multiplication.s
@@ -1,107 +1,31 @@
 ADDI x5, x0, 16         # x5 = 16
+ADDI x1, x0, 16
+ADDI x2, x0, 16
+ADDI x2, x2, 16
 vsetivli x20, x5, 18    # 32 bit elements 512-bit vector
-ADDI x1, x0, 16
-ADDI x2, x0, 32
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
 vle32.v v0, x0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-vle32.v v4, x1
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
+vle32.v v4, x5
 vmul.vv v8, v0, v4
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
 vse32.v v8, x0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
 vsetivli x20, x5, 10    # 16 bit elements 512-bit vector
-ADDI x0, x0, 0
-ADDI x0, x0, 0
 vmul.vv v8, v0, v4
-ADDI x1, x0, 16
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
 vse32.v v8, x1
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
 vsetivli x20, x5, 2     # 8 bit elements 512-bit vector
-ADDI x0, x0, 0
-ADDI x0, x0, 0
 vmul.vv v8, v0, v4
-ADDI x2, x0, 32
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
 vse32.v v8, x2
-ADDI x5, x0, 80
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-ADDI x0, x0, 0
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
-C.NOP
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
